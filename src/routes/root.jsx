a386a6191870028271efd03d685119a08713880d
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2024-06-27 22:31:26 +0200

    Search

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,6 @@
-import { Outlet, Link, useLoaderData, Form, redirect, NavLink, useNavigation} from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink, useNavigation, useSubmit} from "react-router-dom";
 import "./root.css"
-
+import { useEffect } from "react";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -8,26 +8,37 @@ export async function action() {
   return redirect("/portal/contacts/" + contact.id + "/edit");
 }
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+
+
+    useEffect(() => {
+      document.getElementById("q").value = q;
+    }, [q]);
 
     return (
       <>
         <div id="sidebar">
           <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
+                onChange={(event) => {
+                  submit(event.currentTarget.form);
+                }}
               />
               <div
                 id="search-spinner"
@@ -38,7 +49,7 @@ export default function Root() {
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
